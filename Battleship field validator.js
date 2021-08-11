0757fdb278220f691e94a89ffc2293260bd558e8
Ilia <43654815+istudyatuni@users.noreply.github.com>
2021-08-11 11:13:39 +0300

    Update Battleship field validator.js

--- Battleship field validator.js
+++ Battleship field validator.js
@@ -1,30 +1,41 @@
-// here not count number of ships
-
 // square - [[1, 0], [0, 0]]
 function validateWindow(square) {
-  let sum = 0;
-  square.map(e => e).forEach(e => sum += e[0] + e[1])
+  let sum = square.reduce((acc, e) => acc + e[0] + e[1], 0)
   switch(sum) {
-    case 1: return true; break;
-    case 3: case 4: return false; break;
+    case 1: return sum;
+    case 3: case 4: return -1;
   }
   // bad when in diagonals
   if(square[0] === [1, 0] && square[1] === [0, 1] ||
      square[0] === [0, 1] && square[1] === [1, 0]) {
-    return false;
+    return -1;
   }
-  return true;
+  return sum;
+}
+
+let copy
+
+// i, j is coordinates of the top left corner
+function countShips(square, countWindow, i, j) {
+  if(countWindow !== 1 && countWindow !== 2) return;
+  let sq = [...square[0], ...square[1]]
+  let indexes = sq.findIndex(1) // not finished
 }
 
 function validateBattlefield(field) {
+  copy = field.map(e => e.map(e => e === 1 ? -1 : e))
+
   let sq = [[0, 0], [0 ,0]];
+  let countWindow
   for(let i = 0; i < 8; i++) {
     for(let j = 0; j < 8; j++) {
       sq = [[field[i][j],     field[i][j + 1]],
             [field[i + 1][j], field[i + 1][j + 1]]]
-      if(!validateWindow(sq)) {
+      countWindow = validateWindow(sq)
+      if(countWindow === -1) {
         return false;
       }
+      countShips(sq, countWindow, i, j)
     }
   }
   return true;
